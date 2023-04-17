be03638f9a8a55f92bb11b2e7742515f3d1774e4
DevOps2906 <huntinghusk29@gmail.com>
2023-04-17 20:24:29 +0530

    start

--- whylogistics/settings.py
+++ whylogistics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!#_he7(1$6fp57md=9ea%#r8l2#$75352a_c=8#gyl2(b^+arc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
